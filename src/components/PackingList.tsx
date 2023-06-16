bf504cb10f88494534631868f523ce09b7599199
Abdul Rehman <r3hmantom@gmail.com>
2023-06-16 21:21:59 +0500

    PackingList component

--- src/components/PackingList.tsx
+++ src/components/PackingList.tsx
@@ -1,5 +1,42 @@
-const PackingList = () => {
-  return <div className="list">LIST</div>;
+import React from "react";
+
+interface ItemProps {
+  item: {
+    id: number;
+    description: string;
+    quantity: number;
+    packed: boolean;
+  };
+}
+
+// ITEM LIST
+const PackingList: React.FC = () => {
+  const initialItems: ItemProps["item"][] = [
+    { id: 1, description: "Passports", quantity: 2, packed: false },
+    { id: 2, description: "Socks", quantity: 12, packed: true },
+  ];
+
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => {
+          return <Item item={item} />;
+        })}
+      </ul>
+    </div>
+  );
+};
+
+// ITEM
+const Item: React.FC<ItemProps> = ({ item }) => {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 };
 
 export default PackingList;
